e1bc9fbb7b81af4470b8fed50bfc7fee65911b6e
Christian Lewis <christianlewis024@gmail.com>
2020-10-18 12:01:22 -0700

    more styling on sidebar, hover effects and borders and scrollbar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -10,29 +10,77 @@ function Sidebar() {
     return (
         <div className="sidebar">
             <div className="sidebar__top">
+                <div className="sidebar__greeting">
                 <h2> Welcome Christian</h2>
+                </div>
+                <div className="sidebar__links">
                 <h2>Your Profile</h2>
+                </div>
+                <div className="sidebar__links">
                 <h2>New Post + </h2>
+                </div>            
             </div>
             <div className="sidebar__bottom">
                 <div className="sidebar__inbox">
-                <Collapsible  className="sidebar__collapsible" trigger={<div className="inbox__collapsibleHeader">Your Inbox  <MailOutlineIcon/></div>}  triggerlazyRender="true"
+                
+                <Collapsible  className="sidebar__collapsible" trigger={<div className="sidebar__collapsibleHeader"><h5>Your Inbox  <MailOutlineIcon/></h5></div>}  triggerlazyRender="true"
             transitionTime={200}>
-                <h5>Send Private Message <AddCircleOutlineIcon/></h5>
+                <h4 className="sidebar__borderBottom">New Message <AddCircleOutlineIcon/></h4>
+                
+                <div className="sidebar__inboxItems">
                     <h5 className="sidebar__inboxOption">Christian & Robby</h5>
                     <h5 className="sidebar__inboxOption">Christian & Bob</h5>
                     <h5 className="sidebar__inboxOption">Christian & Billy</h5>
                     <h5 className="sidebar__inboxOption">Christian & Johnny</h5>
+                    <h5 className="sidebar__inboxOption">Christian & Robby</h5>
+                    <h5 className="sidebar__inboxOption">Christian & Bob</h5>
+                    <h5 className="sidebar__inboxOption">Christian & Billy</h5>
+                    <h5 className="sidebar__inboxOption">Christian & Johnny</h5>
+                    <h5 className="sidebar__inboxOption">Christian & Robby</h5>
+                    <h5 className="sidebar__inboxOption">Christian & Bob</h5>
+                    <h5 className="sidebar__inboxOption">Christian & Billy</h5>
+                    <h5 className="sidebar__inboxOption">Christian & Johnny</h5>
+                    <h5 className="sidebar__inboxOption">Christian & Robby</h5>
+                    <h5 className="sidebar__inboxOption">Christian & Bob</h5>
+                    <h5 className="sidebar__inboxOption">Christian & Billy</h5>
+                    <h5 className="sidebar__inboxOption">Christian & Johnny</h5>
+                    <h5 className="sidebar__inboxOption">Christian & Robby</h5>
+                    <h5 className="sidebar__inboxOption">Christian & Bob</h5>
+                    <h5 className="sidebar__inboxOption">Christian & Billy</h5>
+                    <h5 className="sidebar__inboxOption">Christian & Johnny</h5>
+                    <h5 className="sidebar__inboxOption">Christian & Robby</h5>
+                    <h5 className="sidebar__inboxOption">Christian & Bob</h5>
+                    <h5 className="sidebar__inboxOption">Christian & Billy</h5>
+                    <h5 className="sidebar__inboxOption">Christian & Johnny</h5>
+                    </div>
                 </Collapsible>
                 </div>
                 <div className="sidebar__rooms">
-                <Collapsible  className="sidebar__collapsible" trigger={<div className="inbox__collapsibleHeader">Public Rooms  <MailOutlineIcon/></div>} triggerlazyRender="true"
+                <Collapsible  className="sidebar__collapsible" trigger={<div className="sidebar__collapsibleHeader"><h5>Public Rooms  <GroupOutlinedIcon/></h5></div>} triggerlazyRender="true"
             transitionTime={200}>
-                <h5 className="sidebar__roomHeader">Create a new room <GroupAddOutlinedIcon/></h5>
+                <h4 className="sidebar__borderBottom">Create a new room <GroupAddOutlinedIcon/></h4>
+                <div className="sidebar__roomItems">
                     <h5 className="sidebar__inboxOption">General chat</h5>
                     <h5 className="sidebar__inboxOption">Trade Chat</h5>
                     <h5 className="sidebar__inboxOption">Local Defense</h5>
                     <h5 className="sidebar__inboxOption">Guild Recruitment</h5>
+                    <h5 className="sidebar__inboxOption">General chat</h5>
+                    <h5 className="sidebar__inboxOption">Trade Chat</h5>
+                    <h5 className="sidebar__inboxOption">Local Defense</h5>
+                    <h5 className="sidebar__inboxOption">Guild Recruitment</h5>
+                    <h5 className="sidebar__inboxOption">General chat</h5>
+                    <h5 className="sidebar__inboxOption">Trade Chat</h5>
+                    <h5 className="sidebar__inboxOption">Local Defense</h5>
+                    <h5 className="sidebar__inboxOption">Guild Recruitment</h5>
+                    <h5 className="sidebar__inboxOption">General chat</h5>
+                    <h5 className="sidebar__inboxOption">Trade Chat</h5>
+                    <h5 className="sidebar__inboxOption">Local Defense</h5>
+                    <h5 className="sidebar__inboxOption">Guild Recruitment</h5>
+                    <h5 className="sidebar__inboxOption">General chat</h5>
+                    <h5 className="sidebar__inboxOption">Trade Chat</h5>
+                    <h5 className="sidebar__inboxOption">Local Defense</h5>
+                    <h5 className="sidebar__inboxOption">Guild Recruitment</h5>
+                    </div>
                 </Collapsible>
                 </div>
             </div>
