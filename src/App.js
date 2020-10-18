5301e9cb5dfcda1edb409c3cf2bf5b57c3770689
Christian Lewis <christianlewis024@gmail.com>
2020-10-18 14:54:38 -0700

    login and signup working, need to style login

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
-import React from 'react';
+import React, { useEffect } from "react";
 import './App.css';
 import Header from "./Header"
 import Sidebar from "./Sidebar"
 import Feed from "./Feed"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./Login";
+import { useStateValue } from "./StateProvider";
+import {auth} from "./firebase"
+
 function App() {
+  const [{user}, dispatch] = useStateValue();
+  useEffect(() => {
+    // will only run once when the app component loads...
+
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>> ", authUser);
+
+      if (authUser) {
+        // the user just logged in / the user was logged in
+
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
   return (
     <div className="app">    
-      
-      <Header/>
-      <div className="app__body">
-      <div className="app__banner">
-        <img src="https://images.unsplash.com/photo-1592185152497-03502ba153ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" alt=""/>
-        </div>
-        <Sidebar/>
-        <Feed/>
-      </div>
-      
+      <Router>
+      {!user ? (
+          <Login/>
+        ) : (
+          <>
+        <Header/>
+              <div className="app__body">
+              <div className="app__banner">
+                <img src="https://images.unsplash.com/photo-1592185152497-03502ba153ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" alt=""/>
+                </div>
+                <Sidebar/>
+                <Feed/>
+              </div>
+          </>
+          )}
+     
+      </Router>
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import "./Header.css"
 import {Avatar} from "@material-ui/core"
+import {useStateValue} from "./StateProvider"
+import { auth } from './firebase'
+import { Link } from 'react-router-dom'
 
 
 function Header() {
+    const [{ basket, user }, dispatch] = useStateValue();
+    const login = () => {
+        if (user) {
+            auth.signOut();
+        }
+    }
     return (
         <div className="header">
            <div className="header__left">
@@ -24,14 +33,18 @@ function Header() {
                <div className="header__option">
                    <h4>Posts</h4>
                </div>
+               <div className="header__option">
+               <Link to={!user && "/login"} className="header__link">
+            <div onClick={login}>           
+            <span>{user ? 'Log Out' : 'Sign In'}</span>
+            </div>
+            </Link>
+            </div>
                
            </div>
            <div className="header__right">
                
-               <div className="header__login">
-               <h5>Log In</h5>
-               <h3>Sign Up</h3>
-               </div>
+           
                <div className="header__avatar">
                <Avatar
                src="https://pbs.twimg.com/profile_images/1289438305969254402/UBOYNi2s_400x400.jpg"/>
