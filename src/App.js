f6e22151111263071ce12d5c4a084a95262eef65
Christian Lewis <christianlewis024@gmail.com>
2020-10-19 16:26:18 -0700

    comments and styling in place

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import './App.css';
 import Header from "./Header"
 import Sidebar from "./Sidebar"
-import Feed from "./Feed"
+import TextInput from "./TextInput"
+import PostInput from "./PostInput"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./Login";
 import { useStateValue } from "./StateProvider";
-import {auth} from "./firebase"
+import {auth, db} from "./firebase"
+import Post from "./Post"
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 function App() {
   const [{user}, dispatch] = useStateValue();
+  const [posts, setPosts] = useState([]);
+
   useEffect(() => {
     // will only run once when the app component loads...
 
@@ -32,6 +37,25 @@ function App() {
       }
     });
   }, []);
+
+
+
+  useEffect(() => {
+    // code runs here
+    db.collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setPosts(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            post: doc.data(),
+          }))
+        );
+      });
+   
+  }, []);
+
+
   return (
     <div className="app">    
       <Router>
@@ -43,9 +67,38 @@ function App() {
               <div className="app__body">
               <div className="app__banner">
                 <img src="https://images.unsplash.com/photo-1592185152497-03502ba153ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" alt=""/>
-                </div>
+              </div>
                 <Sidebar/>
-                <Feed/>
+                <div className="app__posts">
+             
+                <Tabs>
+                <TabList>
+                  <div className="feed__tabs">
+                    <Tab>Text Post & GIF</Tab>
+                    <Tab>Photo Upload</Tab>
+                    </div>
+                </TabList>
+                <TabPanel>
+                <h2><TextInput/></h2>
+                </TabPanel>
+                <TabPanel>
+                <PostInput/>
+                </TabPanel>
+            </Tabs>
+                {posts.map(({ id, post }) => (
+            <Post
+              user={user}
+              key={id}
+              postId={id}
+              username={post.username}
+              caption={post.caption}
+              imageUrl={post.imageUrl}
+              userImage={post.userImage}
+              timestamp={post.timestamp}
+            />
+          ))}
+          </div>
+                
               </div>
           </>
           )}
